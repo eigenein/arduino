b9509d62a05df0f471e1542d6d7244772503414a
Pavel Perestoronin <eigenein@gmail.com>
2014-12-08 00:07:06 +0300

    Time correction

--- clock/src/sketch.ino
+++ clock/src/sketch.ino
@@ -3,15 +3,30 @@
 LiquidCrystal lcd(4, 5, 10, 11, 12, 13);
 
 void setup() {
+    Serial.begin(9600);
     lcd.begin(16, 2);
 }
 
 void loop() {
+    static unsigned long time_offset = 0;
     static const char spin[] = "\x80\x81\x82\x83";
 
-    const unsigned long time = millis() / 1000;
+    const unsigned long relative_time = millis() / 1000;
 
-    const int hours = time / 3600;
+    // Time correction via serial port.
+    if (Serial.available() != 0) {
+        char current_time_string[16];
+        if (Serial.readBytesUntil('\n', current_time_string, sizeof(current_time_string)) != 0) {
+            unsigned long current_time;
+            if (sscanf(current_time_string, "%ld", &current_time) == 1) {
+                time_offset = current_time - relative_time;
+            }
+        }
+    }
+
+    const unsigned long time = relative_time + time_offset;
+
+    const int hours = (time / 3600) % 24;
     const int minutes = (time / 60) % 60;
     const int seconds = time % 60;
 
